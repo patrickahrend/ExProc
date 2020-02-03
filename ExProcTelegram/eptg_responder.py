0225152456224a0f24d77f22288dd1f7b58b5326
FarhadGSRX <farhad.ghamsari@gmail.com>
2020-02-02 18:25:38 -0600

    Corrected how I was handing absolute imports for eptg.

--- ExProcTelegram/eptg_responder.py
+++ ExProcTelegram/eptg_responder.py
@@ -4,11 +4,12 @@ responses = {"hello":"hey!"}
 def messenger(message_text):
     #Higher Level Functions first?
 
+    the_msg = message_text.lower()
     if message_text.lower() == "reboot raspi":
         reboot_raspi()
         return "Rebooting Raspberry Pi Now."
-    if message_text.lower() in responses.keys():
-        return responses[message_text]
+    if the_msg in responses.keys():
+        return responses[the_msg]
     else:
         return "Command not recognized."
 
